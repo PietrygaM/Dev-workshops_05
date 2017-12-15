377f213533a25b1ee6d98950f69814df7e40fb54
Marcin Pietryga <pietryga.marcin@gmail.com>
2017-12-15 15:06:36 +0100

    app config and homecontroller

---
+++ src/main/java/pl/coderslab/bean/Book.java
@@ -0,0 +1,21 @@
+package pl.coderslab.bean;
+
+public class Book {
+
+	private long id;
+	private String isbn;
+	private String title;
+	private String author;
+	
+	
+	
+	
+	id: long,
+	isbn: String,
+	title: String,
+	author: String,
+	publisher: String,
+	type: String,
+	
+	
+}
